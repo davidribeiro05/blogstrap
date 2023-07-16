4ecdf0ca449342ef6181c9f715ff60a9dd9aad40
David Ribeiro <david.ribeiro05@hotmail.com>
2023-07-16 16:48:50 -0300

    feat: implementação tooltipe

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,12 +3,29 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+require("@popperjs/core")
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import { Tooltip, Toast, Popover } from 'bootstrap'
 import 'bootstrap/dist/js/bootstrap'
 import "bootstrap/dist/css/bootstrap";
+
+// If you're using Turbolinks. Otherwise simply use: jQuery(function () {
+document.addEventListener("turbolinks:load", () => {
+    // Both of these are from the Bootstrap 5 docs
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new Tooltip(tooltipTriggerEl)
+    })
+
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+        return new Popover(popoverTriggerEl)
+    })
+})
 
 Rails.start()
 Turbolinks.start()
